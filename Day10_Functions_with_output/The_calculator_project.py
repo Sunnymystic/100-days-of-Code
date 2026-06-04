c1c434b56c1821cd4ead4e0cf007ed54c1df8e08
Dogra <sunny.dogra@capgemini.com>
2026-06-04 18:32:33 +0530

    Final version of the Calculator Project

--- Day10_Functions_with_output/The_calculator_project.py
+++ Day10_Functions_with_output/The_calculator_project.py
@@ -13,34 +13,35 @@ def divide(n1,n2):
     return n1/n2
 
 operations = {
-    "+":add,
-    "-":subtract,
-    "*":multiply,
-    "/":divide,
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
 }
-repeat = 'n'
-while repeat == 'n' or repeat == 'y':
-    if repeat == 'n':
+
+first_num = float(input("What's the first number? "))
+while True:
+    for operation in operations:
+        print(operation)
+
+    operation = input("Pick an operation: ")
+    if operation not in operations:
+        print("Invalid operation")
+        continue
+
+    second_num = float(input("What's the next number?: "))
+    if operation == "/" and second_num == 0:
+        print("Error: division by zero is not allowed.")
+        continue
+
+    result = operations[operation](first_num, second_num)
+    print(f"{first_num} {operation} {second_num} = {result}")
+
+    next_step = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+    if next_step == "y":
+        first_num = result
+    elif next_step == "n":
         first_num = float(input("What's the first number? "))
     else:
-        first_num = float(result)
-    ops = list(operations.keys())
-    print(ops[0])
-    print(ops[1])
-    print(ops[2])
-    print(ops[3])
-    operation = input("Pick an operation: ")
-    if operation not in ops:
-        print("Invalid opeation")
-        exit()
-    second_num = float(input("What's the next number?: "))
-    if operation == "+":
-        result = operations["+"](first_num,second_num)
-    elif operation == "-":
-        result = operations["-"](first_num,second_num)
-    elif operation == "*":
-        result = operations["*"](first_num,second_num)
-    elif operation == "/":
-        result = operations["/"](first_num,second_num)
-    print(f"{first_num} {operation} {second_num} = {result}")        
-    repeat = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+        print("Invalid choice. Exiting.")
+        break
