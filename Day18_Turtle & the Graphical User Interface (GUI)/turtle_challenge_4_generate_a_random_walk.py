5c9cc614e4f8ba243e2e31c9a3dc583f2752a02f
Dogra <sunny.dogra@capgemini.com>
2026-06-12 19:19:29 +0530

    added randomness in the color using rgb combination.

--- Day18_Turtle & the Graphical User Interface (GUI)/turtle_challenge_4_generate_a_random_walk.py
+++ Day18_Turtle & the Graphical User Interface (GUI)/turtle_challenge_4_generate_a_random_walk.py
@@ -2,16 +2,25 @@
 #TODO: Pen should be of thicker size.
 #TODO: Each line should be of different color
 #TODO: Increase the speed of turtle gradually as it moves.
-from turtle import Turtle,Screen
+from turtle import Screen
+import turtle as t
 import random
 
-tim = Turtle()
+t.colormode(255)
+tim = t.Turtle()
 tim.pensize(20)
-color = ["red","blue","gold","pink","purple","magenta","black","#800020","olive","brown"]
 
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    color = (r,g,b)
+    return color
+    
+   
 directions = [0,90,180,270]
 for i in range(1000):
-    tim.pencolor(random.choice(color))
+    tim.pencolor(random_color())
     tim.forward(25)
     tim.setheading(random.choice(directions))
     tim.speed("fastest")
